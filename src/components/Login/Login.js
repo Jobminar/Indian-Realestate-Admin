606fd6e76b7ef0337a720a2a946a2ef99e1a1086
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-01-13 19:07:37 +0530

    sameer added admin data to session storage

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import { TextField, Checkbox, FormControlLabel, Button } from '@mui/material';
-import '../Login/login.css';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { TextField, Checkbox, FormControlLabel, Button } from "@mui/material";
+import "../Login/login.css";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
     rememberMe: false,
   });
 
@@ -23,37 +23,44 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('https://raddaf-be.onrender.com/admin/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        "https://raddaf-be.onrender.com/admin/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
 
       const data = await response.json();
 
       if (response.ok) {
         alert(`hi ${formData.email} welcome to login`);
-        console.log('Login successful:', data);
-        navigate('/');
+        console.log("Login successful:", data);
+
+        // Store agent data in session storage
+        sessionStorage.setItem("agentData", JSON.stringify(data.admin));
+
+        navigate("/");
         // Handle successful login, e.g., redirect to another page
       } else {
-        console.error('Login failed:', data.error);
-        alert('Login failed', data.error);
+        console.error("Login failed:", data.error);
+        alert("Login failed", data.error);
         // Handle failed login, e.g., display an error message
       }
     } catch (error) {
-      console.error('Error during login:', error);
+      console.error("Error during login:", error);
     }
 
-    console.log('Login clicked', formData);
+    console.log("Login clicked", formData);
     // alert(`hi ${formData.email} welcome to login`);
   };
 
   return (
-    <div className='main-divs'>
-      <form className='form-contain' onSubmit={handleSubmit}>
+    <div className="main-divs">
+      <form className="form-contain" onSubmit={handleSubmit}>
         <h2>Admin Login Form</h2>
         <TextField
           variant="filled"
@@ -87,7 +94,7 @@ const Login = () => {
           }
           label="Remember Me"
         />
-        <Button variant='contained' type='submit' className='button-admin'>
+        <Button variant="contained" type="submit" className="button-admin">
           Login
         </Button>
       </form>
